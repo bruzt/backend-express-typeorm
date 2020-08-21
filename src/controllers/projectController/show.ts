a0414819687df4426ec22c9e21f5e2281869bece
Bruno Zutim <bzutim@gmail.com>
2020-08-21 15:39:09 -0300

    add project controller validators

--- src/controllers/projectController/show.ts
+++ src/controllers/projectController/show.ts
@@ -9,12 +9,18 @@ export default async function show(req: Request, res: Response){
     try {
 
         const project = await ProjectModel.findOne(id, { 
-            relations: ['usersProjects']
+            relations: ['usersProjects', 'usersProjects.user']
         });
 
         if(!project) return res.status(400).json({ message: 'Project not found' });
 
-        project.usersProjects.forEach( (userProject) => delete userProject.project);
+        if(project.usersProjects && project.usersProjects.length > 0){
+
+            project.usersProjects.forEach( (userProject) => {
+                delete userProject.user.password;
+                delete userProject.user.tempPassword;
+            });
+        }
 
         return res.json(project);
         
---
+++ src/validators/projectControllerValidator.ts
@@ -0,0 +1,27 @@
+import { celebrate, Joi, Segments } from 'celebrate';
+
+export default {
+
+    show: celebrate({
+
+        [Segments.PARAMS]: Joi.object().keys({
+
+            id: Joi.number().required()
+        }),
+
+    }, {
+        abortEarly: false
+    }),
+
+    store: celebrate({
+
+        [Segments.BODY]: Joi.object().keys({
+
+            title: Joi.string().required(),
+            description: Joi.string().required(),
+        }),
+
+    }, {
+        abortEarly: false
+    }),
+}
